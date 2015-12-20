23f5b0b6c0e94c4a7ce89b7591f09d418c2e36d5
Meya Stephen Kenigbolo <expensivestephen@hotmail.com>
2015-12-20 20:12:39 +0200

    Test case

---
+++ RubyOrNotRuby.rb
@@ -0,0 +1,10 @@
+#comment
+print "Enter a value: "
+
+first_num = gets.to_i
+
+print "Enter another value: "
+
+second_num = gets.to_i
+
+puts first_num.to_s + " + " + second_num.to_s + " = " (first_num + second_num).to_s
